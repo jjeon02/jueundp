878de0bc20b17ed625ab2bb3d94440651d130aa8
Jueun Jeon <jjeon02@risd.edu>
2020-04-26 15:01:49 +0900

    SAHDSFGHJ Struggle

--- timeline.js
+++ timeline.js
@@ -1,14 +1,10 @@
-document.addEventListener('wheel', function(e)
-{
-  if(e.type != 'wheel')
-  {
-    return;
-  }
-  let delta = ((e.deltaY || -e.wheelDelta || e.detail) >> 10) || 1;
-  delta = delta * (-300);
-  document.documentElement.scrollLeft -= delta;    
-  // safari needs also this
-  document.body.scrollLeft -= delta;    
-  e.preventDefault();
-  
-});
+let move = 1;
+const el = document.getElementById('outer-block');
+el.onwheel = moveRight;
+
+function moveRight(event){
+  event.preDefault();
+  move += event.deltaY * - 0.01;
+  move = Math.min(Math.max(.125,move),4);
+  el.style.transform = 'move(${move})'; 
+}
